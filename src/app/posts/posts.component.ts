737214fc900fc9233e6be1dc1c167b055f786b98
Charles Burch <charlesburch@CBMacBook-Pro.local>
2017-12-16 10:42:51 -0600

    updatePost created

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -18,8 +18,17 @@ export class PostsComponent  {
       });
   }
   createPost(input: HTMLInputElement) {
-    const post = {title : input.value};
+    const post: any = {title : input.value};
+    input.value = '';
     this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post.id = response.json().id;
+        this.posts.unshift(post);
+        console.log(response.json());
+      });
+  }
+  updatePost(post) {
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true}))
       .subscribe(response => {
         console.log(response.json());
       });
